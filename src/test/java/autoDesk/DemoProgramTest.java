4ef984f86eb3ea22b619e876cf96de5579ff4d1a
Abhishek88848 <abhishekkelusker001@gmail.com>
2025-01-26 20:52:42 +0530

    2nd Commit and Push By Abhishek kelusker

---
+++ src/test/java/autoDesk/DemoProgramTest.java
@@ -0,0 +1,14 @@
+package autoDesk;
+
+import org.testng.annotations.Test;
+
+public class DemoProgramTest 
+{
+	
+	@Test
+	public void FirstProgram()
+	{
+		System.out.println("This is my first program");
+	}
+
+}
