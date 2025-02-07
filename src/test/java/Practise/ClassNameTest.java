68d4ffbacf70741ceda6e40a9bc7102432b0d79d
ADMIN <ADMIN@DESKTOP-QR0VDMM>
2025-02-07 13:15:30 +0530

    first commit and push by AT Engineer-ClassNameTest

---
+++ src/test/java/Practise/ClassNameTest.java
@@ -0,0 +1,14 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+public class ClassNameTest 
+{
+	
+	@Test
+	public void classNameTest()
+	{
+		System.out.println("class name test printing statement");
+	}
+
+}
